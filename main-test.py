ecfc12b109d1fa9e32391f2d0514f5b4b3dbfdf4
behzad-shafique <behzad.shafique@empglabs.com>
2022-01-14 16:32:05 +0500

    Update main-test.py

--- main-test.py
+++ main-test.py
@@ -2,7 +2,7 @@ from ast import Assert
 from main import Add
 
 def testAdd():
-    Assert(Add(2,3)==5)
+    Assert(Add(5,5)==10)
     print("Function runs perfectly")
 
 if __name__ == '__main__':  
